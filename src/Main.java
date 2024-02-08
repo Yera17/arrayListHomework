d52b8f76fe57956fd3b8d5a471fa6de2aa76fa16
Yera17 <erko.salaly@gmail.com>
2024-02-08 22:02:20 +0100

    v2

--- src/Main.java
+++ src/Main.java
@@ -5,40 +5,45 @@ import static java.lang.Integer.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList <String> numbers = new ArrayList<>();
+        ArrayList <Integer> numbers = new ArrayList<>();
         while(true) {
             System.out.print("Введите число: ");
-            numbers.add(scanner.nextLine());
-                for(String number : numbers){
-                    if (number.equals("")) {
-                        numbers.remove("");
-                        System.out.printf(""" 
-                                        Ваш список чисел: %s
-                                        Количество чисел: %d
-                                        Наибольшее число: %d
-                                        Наименшее число: %d
-                                        Среднее число: %d
-                                        """, numbers, numbers.size(), getMaxValue(numbers), getMinValue(numbers), getAverageValue(numbers));
-
-                        return;
-                    }
+            String s = scanner.nextLine();
+            if (s.isEmpty()) {
+                numbers.add(s.hashCode());
+            } else {
+                numbers.add(parseInt(s));
+            }
+            for(int i = 0; i < numbers.size(); i++){
+                if (numbers.get(i) == "".hashCode()) {
+                    numbers.remove(numbers.get(i));
+                    System.out.printf(""" 
+                                      Ваш список чисел: %s
+                                      Количество чисел: %d
+                                      Наибольшее число: %d
+                                      Наименшее число: %d
+                                      Среднее число: %d
+                                      """,
+                            numbers, numbers.size(), getMaxValue(numbers), getMinValue(numbers), getAverageValue(numbers));
+                    return;
                 }
+            }
         }
     }
-    public static int getMaxValue(ArrayList<String> arrayList){
+    public static int getMaxValue(ArrayList<Integer> arrayList){
         int max = MIN_VALUE;
         for (int i = 0; i < arrayList.size(); i++) {
-            int value = parseInt(arrayList.get(i));
+            int value = arrayList.get(i);
             if (value > max) {
                 max = value;
             }
         }
         return max;
     }
-    public static int getMinValue(ArrayList<String> arrayList){
+    public static int getMinValue(ArrayList<Integer> arrayList){
         int min = MAX_VALUE;
         for (int i = 0; i < arrayList.size(); i++) {
-            int value = parseInt(arrayList.get(i));
+            int value = arrayList.get(i);
             if (value < min) {
                 min = value;
             }
@@ -46,10 +51,10 @@ public class Main {
         return min;
     }
 
-    public static int getAverageValue(ArrayList<String> arrayList){
+    public static int getAverageValue(ArrayList<Integer> arrayList){
         int average = 0;
         for (int i = 0; i < arrayList.size(); i++) {
-            int value = parseInt(arrayList.get(i));
+            int value = arrayList.get(i);
             average += value;
         }
         return average / arrayList.size();
